dc8ec73f01e8d52c8cfa3375c59e74008314a867
Danyel Clinário <52963268+DanyelC@users.noreply.github.com>
2022-05-22 19:05:43 +0000

    time fix

--- TP2/README.md
+++ TP2/README.md
@@ -1,5 +1,2 @@
 gcc produtor_consumidor.c -o teste -lpthread -lrt -lm && ./teste 32 1 1
 
-  finish = clock();
-  FILE *file = fopen("resultados.csv", "a");
-  fprintf(file, "%f\n", ((double)(finish - start)) / CLOCKS_PER_SEC );
--- TP2/produtor_consumidor.c
+++ TP2/produtor_consumidor.c
@@ -8,38 +8,47 @@
 #include <time.h>
 #include <unistd.h>
 
-// rodar com gcc produtor_consumidor.c -o teste -lpthread -lrt -lm && ./teste 32 1 1
+// rodar com gcc produtor_consumidor.c -o teste -lpthread -lrt -lm && ./teste 32
+// 1 1
 
 /*
   problemas :
 
     acesso a memoria compartilhada --> semaforo mutex
-    o que fazer quando a memória esta cheia/vazia? Checar se esta vazio ou cheio --> semaforo contador
+    o que fazer quando a memória esta cheia/vazia? Checar se esta vazio ou cheio
+  --> semaforo contador
 
 */
 
 /*
-  definir n = tamanho do N_pilha =============================================== OK
+  definir n = tamanho do N_pilha ===============================================
+  OK
 
-  definir nc e np = numero de threads de cada tipo =============================== OK
+  definir nc e np = numero de threads de cada tipo
+  =============================== OK
 
-  produtor gera de 1 - 10^7 e bota na mem  ======================================== OK
+  produtor gera de 1 - 10^7 e bota na mem
+  ======================================== OK
 
   consumidor pega o numero, esvazia o local e verifica se é primo. imprime no
-  terminal o resultado ============================================================= OK
+  terminal o resultado
+  ============================================================= OK
 
-  ordem na mem = semaforos =========================================================== OK
+  ordem na mem = semaforos
+  =========================================================== OK
 
-  se pode entrar no N_pilha ou nao = semaforo contador ================================= OK
+  se pode entrar no N_pilha ou nao = semaforo contador
+  ================================= OK
 
 */
 
-pthread_mutex_t mutexN_pilha; //semaforo mutex p adicionar/apagar da pilha
-sem_t semaforo_livre; //semaforo contador de posições livres
-sem_t semaforo_preenchido; //semaforo contador de posições ocupadas
+pthread_mutex_t mutexN_pilha; // semaforo mutex p adicionar/apagar da pilha
+sem_t semaforo_livre;         // semaforo contador de posições livres
+sem_t semaforo_preenchido;    // semaforo contador de posições ocupadas
 
-int *N_pilha= NULL; // será usado como uma pilha --> facilita o processo
-int contador = 0; // usado para marcar em qual indice deve ser colocado/lido o número
+int *N_pilha = NULL; // será usado como uma pilha --> facilita o processo
+int contador =
+    0; // usado para marcar em qual indice deve ser colocado/lido o número
 int ja_processados = 0; // para limitar o numero de valores consumidos
 int ja_produzidos = 0;
 
@@ -57,13 +66,15 @@ void is_prime(int ni) {
   printf("O %d é primo. WOW!\n", ni);
 }
 
-// precisa verificar quantos numeros ja foram processados, senao vai rodar infinitamente.
+// precisa verificar quantos numeros ja foram processados, senao vai rodar
+// infinitamente.
 void *produzir(void *args) {
   srand(time(NULL));
-  while (ja_processados < (int)pow(10,5)) {
+  while (ja_processados < (int)pow(10, 1)) {
     int r_numb = rand() % (int)pow(10, 7) + 1;
     printf("Produzido: %d\n", r_numb);
-    sem_wait(&semaforo_livre); // só produz se tiver espaço livre. Fica esperando
+    sem_wait(
+        &semaforo_livre); // só produz se tiver espaço livre. Fica esperando
     pthread_mutex_lock(&mutexN_pilha);
     N_pilha[contador] = r_numb;
     contador++;
@@ -73,10 +84,11 @@ void *produzir(void *args) {
   }
 }
 
-//precisa verificar quantos numeros ja foram processados
+// precisa verificar quantos numeros ja foram processados
 void *consumir(void *args) {
-  while (ja_processados < (int)pow(10,1)) {
-    sem_wait(&semaforo_preenchido); // só produz se tiver espaço preenchido. Fica esperando
+  while (ja_processados < (int)pow(10, 1)) {
+    sem_wait(&semaforo_preenchido); // só produz se tiver espaço preenchido.
+                                    // Fica esperando
     pthread_mutex_lock(&mutexN_pilha);
     int ni = N_pilha[contador - 1];
     is_prime(ni); // talvez tirar do semaforo
@@ -85,48 +97,67 @@ void *consumir(void *args) {
     pthread_mutex_unlock(&mutexN_pilha);
     sem_post(&semaforo_livre); // avisa que consumiu
   }
-  //exit(0); // Quando processar 10^5, mata o processo. Roubado dms? --> impossivel pegar o tempo
+  // exit(0); // Quando processar 10^5, mata o processo. Roubado dms? -->
+  // impossivel pegar o tempo
 }
-
 
 int main(int argc, char *argv[]) {
   char *p; // pega valores do terminal
   int N, threads, nc, np;
-  
+  clock_t start, finish;
+  int i = 10;
+  double media = 0;
+  FILE *file = fopen("resultados.csv", "a");
+  fprintf(file, "Combinacao, N, Tempo\n");
+
   N = strtol(argv[1], &p, 10);
   np = strtol(argv[2], &p, 10);
   nc = strtol(argv[3], &p, 10);
-  N_pilha = malloc(N * sizeof(int)); // alocaçao dinamica do tamanho da pilha para facilitar
+  fprintf(file, "%d %d, %d, ", np, nc, N);
+  N_pilha = malloc(
+      N * sizeof(int)); // alocaçao dinamica do tamanho da pilha para facilitar
   threads = nc + np;
-  
-  //Checagem basica
-  if(nc <= 0 || np <= 0 || N<=0){
-    printf("Tamanho da pilha: %d\nNúmero de Threads Totais: %d\nNúmero de Threads Produtoras: %d\nNúmero de Threads Consumidoras: %d\n", N, threads, np, nc);
-    printf("Faz sentido isso? Tem que ter no mínimo 1 Thread Produtora e Consumidora. Ah, o tamanho da pilha tem que ser maior que 0\n");
+  // Checagem basica
+  if (nc <= 0 || np <= 0 || N <= 0) {
+    printf("Tamanho da pilha: %d\nNúmero de Threads Totais: %d\nNúmero de "
+           "Threads Produtoras: %d\nNúmero de Threads Consumidoras: %d\n",
+           N, threads, np, nc);
+    printf("Faz sentido isso? Tem que ter no mínimo 1 Thread Produtora e "
+           "Consumidora. Ah, o tamanho da pilha tem que ser maior que 0\n");
     return 0;
   }
-  
-  printf("Tamanho da pilha: %d\nNúmero de Threads Totais: %d\nNúmero de Threads Produtoras: %d\nNúmero de Threads Consumidoras: %d\n", N, threads, np, nc);
 
-  pthread_t th[threads]; // criando as threads
-  pthread_mutex_init(&mutexN_pilha, NULL);
-  sem_init(&semaforo_livre, 0, N);  //N é qnts estão disponíveis no inicio do código
-  sem_init(&semaforo_preenchido, 0, 0); //0 é qnts estão preenchidos no inicio do código 
-  for (int i = 0; i < threads; i++) {
-    // criando np produtoras. o resto é consumidora
-    if (i < np) { 
-      pthread_create(&th[i], NULL, &produzir, NULL);
-    } else {
-      pthread_create(&th[i], NULL, &consumir, NULL);
+  printf("Tamanho da pilha: %d\nNúmero de Threads Totais: %d\nNúmero de "
+         "Threads Produtoras: %d\nNúmero de Threads Consumidoras: %d\n",
+         N, threads, np, nc);
+
+  while (i > 0) {
+    pthread_t th[threads]; // criando as threads
+    pthread_mutex_init(&mutexN_pilha, NULL);
+    sem_init(&semaforo_livre, 0,
+             N); // N é qnts estão disponíveis no inicio do código
+    sem_init(&semaforo_preenchido, 0,
+             0); // 0 é qnts estão preenchidos no inicio do código
+    for (int i = 0; i < threads; i++) {
+      // criando np produtoras. o resto é consumidora
+      if (i < np) {
+        pthread_create(&th[i], NULL, &produzir, NULL);
+      } else {
+        pthread_create(&th[i], NULL, &consumir, NULL);
+      }
     }
+    for (int i = 0; i < threads; i++) {
+      pthread_join(th[i], NULL);
+    }
+    sem_destroy(&semaforo_livre);
+    sem_destroy(&semaforo_preenchido);
+    pthread_mutex_destroy(&mutexN_pilha);
+    finish = clock();
+    media += ((double)(finish - start)) / CLOCKS_PER_SEC;
+    // fprintf(file, "%f\n", ((double)(finish - start)) / CLOCKS_PER_SEC);
+    i--;
   }
-  for (int i = 0; i < threads; i++) {
-    pthread_join(th[i], NULL);
-  }
-  sem_destroy(&semaforo_livre);
-  sem_destroy(&semaforo_preenchido);
-  pthread_mutex_destroy(&mutexN_pilha);
-  printf("Fábrica Fechada.\n");
+  fprintf(file, "%f\n", media / 10);
+  fclose(file);
   return 0;
 }
-
